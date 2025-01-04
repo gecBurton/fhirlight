5c67c961543e93edd0770ceb307c0880e175a006
gecBurton <g.e.c.burton@gmail.com>
2025-01-04 20:21:28 +0000

    all working

---
+++ api/management/commands/load_example_data.py
@@ -0,0 +1,58 @@
+import json
+import os
+from graphlib import TopologicalSorter
+
+from django.core.management import BaseCommand
+
+from api import serializers
+
+EXAMPLE_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "tests", "data"
+)
+
+
+def extract_reference(document) -> list[str]:
+    references = []
+
+    def _extract_reference(doc):
+        if isinstance(doc, dict):
+            for k, v in doc.items():
+                if k == "reference":
+                    references.append(v.split("/")[-1])
+                else:
+                    _extract_reference(v)
+        elif isinstance(doc, list):
+            for d in doc:
+                _extract_reference(d)
+
+    _extract_reference(document)
+    return references
+
+
+def get_examples() -> list[str]:
+    examples = {}
+    for file_path in os.listdir(EXAMPLE_DIR):
+        with open(os.path.join(EXAMPLE_DIR, file_path)) as f:
+            example = json.load(f)
+        references = extract_reference(example)
+        examples[file_path.removesuffix(".json")] = references
+    graph = TopologicalSorter(examples)
+    return list(graph.static_order())
+
+
+def load_examples():
+    file_names = get_examples()
+    for file_name in file_names:
+        with open(os.path.join(EXAMPLE_DIR, file_name + ".json")) as f:
+            example = json.load(f)
+        resource_type = file_name.split("-")[1] + "Serializer"
+
+        serializer_class = getattr(serializers, resource_type)
+        serializer = serializer_class(data=example)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        load_examples()
---
+++ api/serializers/__init__.py
@@ -0,0 +1,36 @@
+from .patient import PatientSerializer
+from .organization import OrganizationSerializer
+from .practitioner import PractitionerSerializer
+from .operation_outcome import OperationOutcomeSerializer
+from .questionnaire import QuestionnaireSerializer
+from .slot import SlotSerializer
+from .medication import MedicationSerializer
+from .related_person import RelatedPersonSerializer
+from .observation import ObservationSerializer
+from .procedure import ProcedureSerializer
+from .location import LocationSerializer
+from .specimen import SpecimenSerializer
+from .practitioner_role import PractitionerRoleSerializer
+from .schedule import ScheduleSerializer
+from .immunization import ImmunizationSerializer
+from .diagnostic_report import DiagnosticReportSerializer
+
+
+__all__ = [
+    "PatientSerializer",
+    "OrganizationSerializer",
+    "PractitionerSerializer",
+    "OperationOutcomeSerializer",
+    "QuestionnaireSerializer",
+    "SlotSerializer",
+    "MedicationSerializer",
+    "RelatedPersonSerializer",
+    "ObservationSerializer",
+    "ProcedureSerializer",
+    "LocationSerializer",
+    "SpecimenSerializer",
+    "PractitionerRoleSerializer",
+    "ScheduleSerializer",
+    "ImmunizationSerializer",
+    "DiagnosticReportSerializer",
+]
