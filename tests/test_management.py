fb9c65e7610ae596774a3f988d7a273d399a0704
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-01-04 20:29:25 +0000

    all working (#24)

---
+++ tests/test_management.py
@@ -0,0 +1,16 @@
+import pytest
+
+from api.management.commands.load_example_data import get_examples, load_examples
+from api.models.common import BaseProfile
+
+
+def test_get_examples():
+    examples = get_examples()
+    assert examples
+
+
+@pytest.mark.django_db
+def test_load_examples():
+    assert BaseProfile.objects.count() == 0
+    load_examples()
+    assert BaseProfile.objects.count() == 45
