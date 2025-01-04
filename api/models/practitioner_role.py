42ff56bb2a223746c068ffc30abec4e2b7025d63
gecBurton <g.e.c.burton@gmail.com>
2025-01-04 20:16:17 +0000

    added Condition

--- api/models/practitioner_role.py
+++ api/models/practitioner_role.py
@@ -61,7 +61,9 @@ class PractitionerRoleProfile(BaseProfile):
 class PractitionerRoleTelecom(ContactPoint):
     """Contact details that are specific to the role/location/service"""
 
-    practitioner_role = models.ForeignKey(PractitionerRoleProfile, on_delete=models.CASCADE)
+    practitioner_role = models.ForeignKey(
+        PractitionerRoleProfile, on_delete=models.CASCADE
+    )
 
 
 class PractitionerRoleIdentifier(Identifier):
@@ -73,4 +75,6 @@ class PractitionerRoleIdentifier(Identifier):
         choices=SYSTEM,
         help_text="Establishes the namespace for the value - that is, a URL that describes a set values that are unique.",
     )
-    practitioner_role = models.ForeignKey(PractitionerRoleProfile, on_delete=models.CASCADE)
+    practitioner_role = models.ForeignKey(
+        PractitionerRoleProfile, on_delete=models.CASCADE
+    )
