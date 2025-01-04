1becf4a1955cf7587f517359199cad9b7143128e
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-01-04 10:22:06 +0000

    fixed PractitionerRoleProfile name (#22)

--- api/serializers/practitioner_role.py
+++ api/serializers/practitioner_role.py
@@ -1,4 +1,4 @@
-from api.models import PractitionerRole
+from api.models import PractitionerRoleProfile
 from api.models.practitioner_role import (
     PractitionerRoleIdentifier,
     PractitionerRoleTelecom,
@@ -40,4 +40,4 @@ class PractitionerRoleSerializer(ProfileSerializer):
             "code",
             "specialty",
         )
-        model = PractitionerRole
+        model = PractitionerRoleProfile
