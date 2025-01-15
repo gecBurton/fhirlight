024364d600095d2d6006aed1ebbffaea8801f588
gecBurton <g.e.c.burton@gmail.com>
2025-01-15 15:09:06 +0000

    feature/maximal-practitioner

--- api/models/practitioner.py
+++ api/models/practitioner.py
@@ -1,5 +1,15 @@
 from django.db import models
-from api.models.datatypes import ContactPoint, Address, Identifier, Name
+
+from api.fields import PeriodField
+from api.models import OrganizationProfile
+from api.models.datatypes import (
+    ContactPoint,
+    Address,
+    Identifier,
+    Name,
+    DataTypeWithPeriod,
+    Concept,
+)
 from api.models.common import BaseProfile
 
 
@@ -26,6 +36,57 @@ class PractitionerProfile(BaseProfile):
         max_length=8,
         choices=Gender,
         help_text="Administrative Gender - the gender that the person is considered to have for administration and record keeping purposes.",
+    )
+
+    birthDate = models.DateField(
+        null=True, blank=True, help_text="The date on which the practitioner was born"
+    )
+    communication = models.ManyToManyField(
+        Concept,
+        blank=True,
+        limit_choices_to={"valueset": Concept.VALUESET.UK_CORE_HUMAN_LANGUAGE},
+        help_text="A language the practitioner can use in patient communication",
+    )
+
+
+class PractitionerQualification(DataTypeWithPeriod):
+    """The official certifications, training, and licenses that authorize or otherwise pertain to the provision of care
+    by the practitioner. For example, a medical license issued by a medical board authorizing the practitioner to
+    practice medicine within a certain locality.
+    """
+
+    profile = models.ForeignKey(PractitionerProfile, on_delete=models.CASCADE)
+    code = models.ForeignKey(
+        Concept,
+        on_delete=models.CASCADE,
+        limit_choices_to={"valueset": Concept.VALUESET.DEGREE_LICENSE_CERTIFICATE},
+        help_text="Coded representation of the qualification.",
+    )
+    period = PeriodField(
+        null=True,
+        blank=True,
+        help_text="Period during which the qualification is valid.",
+    )
+    issuer = models.ForeignKey(
+        OrganizationProfile,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        help_text="Organization that regulates and issues the qualification.",
+    )
+
+
+class PractitionerQualificationIdentifier(Identifier):
+    """An identifier that applies to this person's qualification in this role"""
+
+    system = models.URLField(
+        max_length=64,
+        help_text="Establishes the namespace for the value - that is, a URL that describes a set values that are unique.",
+    )
+
+    practitioner_qualification = models.ForeignKey(
+        PractitionerQualification,
+        on_delete=models.CASCADE,
     )
 
 
