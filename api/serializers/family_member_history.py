8eacb2588deaca4e10a50277718aedcac4ccd15a
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-01-15 14:38:11 +0000

    family_member_history simplification (#67)

--- api/serializers/family_member_history.py
+++ api/serializers/family_member_history.py
@@ -1,21 +1,9 @@
-from rest_framework.fields import IntegerField, CharField, URLField
-from rest_framework.serializers import Serializer
-
 from api.models import FamilyMemberHistoryProfile
 from api.models.family_member_history import FamilyMemberHistoryCondition
 from api.serializers.common import ProfileSerializer, BaseModelSerializer
 
 
-class FamilyMemberHistoryConditionOnsetSerializer(Serializer):
-    value = IntegerField(required=False, source="onsetAgeValue")
-    unit = CharField(required=False, source="onsetAgeUnit")
-    system = URLField(required=False, source="onsetAgeSystem")
-    code = CharField(required=False, source="onsetAgeCode")
-
-
 class FamilyMemberHistoryConditionSerializer(BaseModelSerializer):
-    onsetAge = FamilyMemberHistoryConditionOnsetSerializer(required=False, source="*")
-
     class Meta:
         fields = ("code", "onsetAge")
         model = FamilyMemberHistoryCondition
